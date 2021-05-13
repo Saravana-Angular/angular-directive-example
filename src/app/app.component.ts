e57aea0e6d6109012f52ad07b99be90fabf5add7
Saravanakumar Ganesh <kgsaravana87o@gmail.com>
2021-05-13 17:29:51 -0400

    directives implemented to complete the requirement

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,16 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
+  showSecret = false;
+  clicks = [];
+
+  toggleSecret() {
+    this.showSecret = !this.showSecret;
+    this.clicks.push(this.clicks.length + 1);
+  }
 }
